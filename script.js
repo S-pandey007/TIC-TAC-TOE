6fdfe528e05a723403a51f393132313de607d15d
S-pandey007 <157733217+S-pandey007@users.noreply.github.com>
2024-06-25 11:15:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// script.js
+
+let boxes = document.querySelectorAll(".box");
+let turn0 = true; // playerX, playerO
+let reset = document.querySelector("#reset");
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText === "") { // Only allow clicking empty boxes
+            if (turn0) { // playerO
+                box.innerText = "O";
+                turn0 = false;
+            } else { // playerX
+                box.innerText = "X";
+                turn0 = true;
+            }
+            box.style.pointerEvents = "none"; // Disable further clicks
+            checkWinner();
+        }
+    });
+});
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            showPopup(pos1Val);
+            return;
+        }
+    }
+};
+
+function showPopup(winner) {
+    const popup = document.getElementById('popup');
+    const winnerMessage = document.getElementById('winner-message');
+    
+    winnerMessage.textContent = `Congratulations, ${winner}!`;
+    popup.classList.remove('hidden');
+}
+
+function restartGame() {
+    location.reload(); // Reload the page to reset the game
+}
+
+document.getElementById('restart').addEventListener('click', restartGame);
+reset.addEventListener('click', restartGame);
